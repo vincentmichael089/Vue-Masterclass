39608942a2f07afc29dd3a3a0002d1dd73644b61
aftermath089 <vincentmichael089@gmail.com>
2020-05-21 04:11:32 +0800

    add ThreadShow component

---
+++ mami-forum/src/components/ThreadShow.vue
@@ -0,0 +1,64 @@
+<template>
+   <div class="col-large push-top">
+    <h1>{{thread.title}}</h1>
+
+    <div class="post-list">
+      <!--
+        @div
+          @binding {string} key key of the post
+      -->
+      <div v-for="postId in thread.posts"
+      v-bind:key="postId"
+           class="post"
+      >
+        <div class="user-info">
+          <a href="#" class="user-name">{{users[posts[postId].userId].name}}</a>
+
+          <a href="#">
+            <!--
+                @img
+                  @binding {string} src src of the image
+              -->
+            <img class="avatar-large" v-bind:src="users[posts[postId].userId].avatar" alt="">
+          </a>
+
+          <p class="desktop-only text-small">107 posts</p>
+        </div>
+
+        <div class="post-content">
+          <div>
+            {{posts[postId].text}}
+          </div>
+        </div>
+
+        <div class="post-date text-faded">
+          {{posts[postId].publishedAt}}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import sourceData from '@/data.json'
+
+export default {
+  props: {
+    id: {
+      type: String,
+      required: true
+    }
+  },
+  data () {
+    return {
+      thread: sourceData.threads[this.id],
+      posts: sourceData.posts,
+      users: sourceData.users
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
