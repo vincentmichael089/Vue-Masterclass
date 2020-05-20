eaa3c089ef904a3ec03016a35010bd55a1afa5be
aftermath089 <vincentmichael089@gmail.com>
2020-05-21 04:45:23 +0800

    rejuvenate HelloWorld component, create ThreadList and ThreadListItem component and its relation

--- mami-forum/src/components/HelloWorld.vue
+++ mami-forum/src/components/HelloWorld.vue
@@ -1,61 +1,30 @@
 <template>
-  <div>
+  <div class="col-full">
+    <h1>Welcome to Mami-forum!</h1>
     <!--
       @div
-        @binding {string} key key of the thread
+        @binding {Object} threads list of threads
     -->
-    <div
-      v-for="thread in threads"
-      v-bind:key="thread.id"
-      class="col-large push-top"
-    >
-      <h1>{{ thread.title }}</h1>
-      <div class="post-list">
-        <!--
-          @div
-            @binding {string} key key of the post
-        -->
-        <div v-for="postId in thread.posts" v-bind:key="postId" class="post">
-          <div class="user-info">
-            <a href="#" class="user-name">{{
-              users[posts[postId].userId].name
-            }}</a>
-            <a href="#">
-              <!--
-                @img
-                  @binding {string} src src of the image
-              -->
-              <img
-                class="avatar-large"
-                v-bind:src="users[posts[postId].userId].avatar"
-                alt
-              />
-            </a>
-            <p class="desktop-only text-small">107 posts</p>
-          </div>
-          <div class="post-content">
-            <div>{{ posts[postId].text }}</div>
-          </div>
-          <div class="post-date text-faded">
-            {{ posts[postId].publishedAt }}
-          </div>
-        </div>
-      </div>
-    </div>
+    <ThreadList v-bind:threads="threads"/>
   </div>
 </template>
 
 <script>
 import sourceData from '@/data.json'
+import ThreadList from '@/components/ThreadList.vue'
 
 export default {
   name: 'HelloWorld',
   data () {
     return {
-      threads: sourceData.threads,
+      // transform data to array
+      threads: Object.values(sourceData.threads),
       posts: sourceData.posts,
       users: sourceData.users
     }
+  },
+  components: {
+    ThreadList
   }
 }
 </script>
---
+++ mami-forum/src/components/ThreadList.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="thread-list">
+    <h2 class="list-title">Threads</h2>  
+    <!--
+      @div
+        @binding {Array} thread thread in threads
+        @binding {string} key key of the post
+    -->
+    <ThreadListItem 
+      v-for="thread in threads" 
+      v-bind:thread="thread"
+      v-bind:key="thread.key">
+    </ThreadListItem>
+  </div>
+</template>
+
+<script>
+import ThreadListItem from '@/components/ThreadListItem'
+
+export default {
+  name: 'ThreadList',
+  components: {
+    ThreadListItem
+  },
+  props: {
+    threads: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
