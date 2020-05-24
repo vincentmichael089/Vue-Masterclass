86bfa8f53db4517143d87bf96abb805a79a82d13
aftermath089 <vincentmichael089@gmail.com>
2020-05-24 16:38:05 +0800

    init firebase

--- mami-forum/src/main.js
+++ mami-forum/src/main.js
@@ -6,8 +6,25 @@ import router from './router'
 import store from './store'
 import AppDate from '@/components/AppDate'
 
+import firebase from 'firebase'
+
 Vue.component('AppDate', AppDate)
 Vue.config.productionTip = false
+
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyCTnPjVXA7aRuA7R-4I_-o43LUG6j1NhVw',
+  authDomain: 'mami-forum.firebaseapp.com',
+  databaseURL: 'https://mami-forum.firebaseio.com',
+  projectId: 'mami-forum',
+  storageBucket: 'mami-forum.appspot.com',
+  messagingSenderId: '424506206449',
+  appId: '1:424506206449:web:b59d30c1ab6b2ba1946150',
+  measurementId: 'G-SHJS098331'
+}
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
 
 /* eslint-disable no-new */
 new Vue({
