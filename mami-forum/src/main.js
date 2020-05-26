76b8b15c5b458502d97fe1e3373a9ca2833bb509
aftermath089 <vincentmichael089@gmail.com>
2020-05-26 09:27:51 +0800

    firebase: move firebase config to .env, and gitignore it

--- mami-forum/src/main.js
+++ mami-forum/src/main.js
@@ -13,14 +13,14 @@ Vue.config.productionTip = false
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyCTnPjVXA7aRuA7R-4I_-o43LUG6j1NhVw',
-  authDomain: 'mami-forum.firebaseapp.com',
-  databaseURL: 'https://mami-forum.firebaseio.com',
-  projectId: 'mami-forum',
-  storageBucket: 'mami-forum.appspot.com',
-  messagingSenderId: '424506206449',
-  appId: '1:424506206449:web:b59d30c1ab6b2ba1946150',
-  measurementId: 'G-SHJS098331'
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 }
 
 // Initialize Firebase
