f342604766dafc29197f326a7d85e09bd82923ce
aftermath089 <vincentmichael089@gmail.com>
2020-05-27 20:32:44 +0800

    navigation guards: PageThreadEdit

--- mami-forum/src/views/PageThreadEdit.vue
+++ mami-forum/src/views/PageThreadEdit.vue
@@ -13,18 +13,19 @@
         @binding {string} text text of the thread
     -->
     <ThreadEditor
+    ref='editor'
     v-bind:title='thread.title'
     v-bind:text='text'
     v-on:save='save'
     v-on:cancel='cancel'
     />
-  </div>
+    <!-- ref is used to get what state of title and text in ThreadEditor since this parent doesn know anything about  it-->  </div>
 </template>
 
 <script>
 import ThreadEditor from '@/components/ThreadEditor'
 import asyncDataStatus from '@/mixins/asyncDataStatus'
-
+import store from '@/store'
 export default {
   components: {
     ThreadEditor
@@ -56,12 +57,27 @@ export default {
       // firstPostId is the first post or the beginning of the thread, so editing it means editing the thread post
       const post = this.$store.state.posts[this.thread.firstPostId]
       return post ? post.text : null
+    },
+
+    hasUnsavedChanges () {
+      // this.saved is not required in this implementation because `this.thread.title` and `this.text` are reactive
+      // Thus `hasUnsavedChanges` will automatically become false when the thread is updated
+      return this.$refs.editor.form.title !== this.thread.title || this.$refs.editor.form.text !== this.text
     }
   },
   created () {
     this.$store.dispatch('fetchThread', {id: this.id})
     .then(thread => this.$store.dispatch('fetchPost', {id: thread.firstPostId}))
     .then(() => this.asyncDataStatus_fetched())
+  },
+
+  beforeRouteLeave (to, from, next) {
+    if (this.hasUnsavedChanges) { // if form has unsaved changes
+      const confirmed = window.confirm('Are you sure you want to leave? Any unsaved changes will be lost!')
+      next(confirmed) // next if confirmed true, stay if confirmed is false
+    } else { // else, there is no difference, can leave safely
+      next()
+    }
   }
 }
 </script>
